b9d24d50d0c40adc2b7a61413718f09a73aea186
swsw <swsw1005@gmail.com>
2020-06-10 00:26:23 +0900

    200609 밤 작업중

--- DB document/01. DB생성.sql
+++ DB document/01. DB생성.sql
@@ -7,7 +7,6 @@ use baemindb;
 
 
  
-
         
 CREATE TABLE member
 (
@@ -44,9 +43,9 @@ CREATE TABLE notice
 (
   no        INT         NOT NULL AUTO_INCREMENT COMMENT 'AUTO_Increment',
   title     VARCHAR(60) NOT NULL,
-  content   TEXT(65535) NULL    ,
-  startDate DATE        NULL    ,
-  endDate   DATE        NULL    ,
+  content   TEXT        NOT NULL,
+  startDate DATE        NOT NULL,
+  endDate   DATE        NOT NULL,
   PRIMARY KEY (no)
 );
 
@@ -58,13 +57,16 @@ CREATE TABLE Order_cancel
   PRIMARY KEY (no)
 );
 
+ALTER TABLE Order_cancel
+  ADD CONSTRAINT UQ_orderlist_no UNIQUE (orderlist_no);
+
 CREATE TABLE orderlist
 (
   no           INT         NOT NULL AUTO_INCREMENT COMMENT 'AUTO_Increment',
   name         varchar(30) NOT NULL,
   orderDate    DATETIME    NOT NULL,
   status       INT         NOT NULL DEFAULT 0,
-  orderList    TEXT(65535) NOT NULL,
+  orderList    TEXT        NOT NULL,
   completeTime DATETIME    NULL    ,
   addr         VARCHAR(90) NOT NULL,
   addr2        VARCHAR(90) NOT NULL,
@@ -92,15 +94,15 @@ CREATE TABLE shop
   pw           VARCHAR(20) NOT NULL,
   shopName     VARCHAR(30) NOT NULL,
   shopCategory VARCHAR(60) NOT NULL,
-  shopEx       TEXT(65535) NULL    ,
+  shopEx       TEXT        NULL    ,
   shopAddr     VARCHAR(90) NOT NULL,
   shopAddr2    VARCHAR(90) NULL    ,
   shopTel      VARCHAR(20) NOT NULL,
   shopX        DOUBLE      NOT NULL,
   shopY        DOUBLE      NOT NULL,
   shopStatus   INT         NOT NULL DEFAULT 0,
-  regDate      DATETIME    NULL    ,
-  grade        INT         NULL     DEFAULT 2,
+  regDate      DATETIME    NOT NULL,
+  grade        INT         NOT NULL DEFAULT 2,
   PRIMARY KEY (no)
 );
 
@@ -137,6 +139,9 @@ ALTER TABLE review
     FOREIGN KEY (member_no)
     REFERENCES member (no);
 
+      
+      
+      
       
 
 insert into member (id, pw, name, tel, addr, addr2, regdate, grade, memberX, memberY)
