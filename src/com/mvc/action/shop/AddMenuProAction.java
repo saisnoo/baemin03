1a0f9a21abe752c1178f68af8990a2df27c3fe3c
swsw <swsw1005@gmail.com>
2020-06-16 15:45:21 +0900

    BBS

--- src/com/mvc/action/shop/AddMenuProAction.java
+++ src/com/mvc/action/shop/AddMenuProAction.java
@@ -1,5 +1,21 @@
 package com.mvc.action.shop;
 
-public class AddMenuProAction {
+import javax.servlet.http.*;
+import com.mvc.action.CommandAction;
+
+public class AddMenuProAction implements CommandAction {
+
+    @Override
+    public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+
+        request.setCharacterEncoding("UTF-8");
+        System.out.println("- - - - AddMenuProAction - - - - ");
+        ///////////////// 이 아래에 내용 넣으세요 /////////////////////////
+
+        //
+
+        ///////////////// 이 위에 내용 넣으세요 /////////////////////////
+        return " aaaaa ";
+    }
 
 }
