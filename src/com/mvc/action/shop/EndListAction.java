ae9ab231be75e74924165c0d60cd3ffb05240e06
swsw <swsw1005@gmail.com>
2020-06-17 11:44:52 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/com/mvc/action/shop/EndListAction.java
+++ src/com/mvc/action/shop/EndListAction.java
@@ -10,22 +10,16 @@ import com.mvc.action.CommandAction;
 
 public class EndListAction implements CommandAction {
 
-    @Override
-    public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+  @Override
+  public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 
-        request.setCharacterEncoding("UTF-8");
-        System.out.println("- - - - EndListAction - - - - ");
-        ///////////////// 이 아래에 내용 넣으세요 /////////////////////////
+    request.setCharacterEncoding("UTF-8");
+    System.out.println("- - - - EndListAction - - - - ");
+    ///////////////// 이 아래에 내용 넣으세요 /////////////////////////
 
-      //자바 구문
-      request.setCharacterEncoding("UTF-8");
-      System.out.println("------EndList.jsp");
-      
-      HttpSession session = ((HttpServletRequest) request).getSession();
-      Object no=session.getAttribute("no");
-      int shopNo=Integer.parseInt(no+""); 
-      System.out.println(request.getParameter("shopNo"));
-      OrderListDAO dao= OrderListDAO.getInstance();
+    // 자바 구문
+    request.setCharacterEncoding("UTF-8");
+    System.out.println("------EndList.jsp");
 
       List<OrderListDTO> orderList = dao.getListFinishToday(shopNo);
       System.out.println(shopNo);
@@ -39,5 +33,14 @@ public class EndListAction implements CommandAction {
         ///////////////// 이 위에 내용 넣으세요 /////////////////////////
         return "/shop/EndList.jsp";
     }
+
+    List<OrderListDTO> orderList = dao.getListFinishToday(shopNo);
+    System.out.println(shopNo);
+
+    request.setAttribute("orderList", orderList);
+    request.setAttribute("shopNo", new Integer(shopNo));
+    ///////////////// 이 위에 내용 넣으세요 /////////////////////////
+    return "/shop/EndList.jsp";
+  }
 
 }
